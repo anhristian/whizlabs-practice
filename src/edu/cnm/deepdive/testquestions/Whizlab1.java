2fc1716d04739a00f49803c7cb272c8321da6944
Anastasia Hristian <an.hristian@gmail.com>
2020-10-27 23:28:14 -0600

    string, StringBuidel example.

---
+++ src/edu/cnm/deepdive/testquestions/Whizlab1.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive.testquestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Whizlab1 {
+
+  public static void main(String[] args) {
+
+    List<String> strings = new ArrayList<>();
+
+    strings.add("A");
+    strings.add("B");
+    strings.add("C");
+
+    String[] arr = new String[2];
+    arr = strings.toArray(arr);
+
+    for (String s : arr) {
+      System.out.print(s);
+    }
+  }
+
+}
