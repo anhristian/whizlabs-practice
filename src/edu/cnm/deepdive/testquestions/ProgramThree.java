e3826fc24c238454d66b06c4c513ba457a233082
Anastasia Hristian <an.hristian@gmail.com>
2020-11-04 23:15:37 -0700

    array example

---
+++ src/edu/cnm/deepdive/testquestions/ProgramThree.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive.testquestions;
+
+public class ProgramThree {
+
+  public static void main(String[] args) {
+
+    String[] x = {"A", "B", "C", "D", "E"};
+    nxt(x);
+    for (String s :x) {
+      System.out.print(s + " ");
+    }
+
+  }
+
+  static void nxt(Object obj) {
+    String[] y = (String[]) obj;
+
+    for (int i = 5, j = 0; i>0; i--, j++) {
+      y[j] = Integer.toString(i);
+    }
+  }
+
+}
