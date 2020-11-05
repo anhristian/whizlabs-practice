287f08e5fec6b4364b710fe16c794c21a80b0136
Anastasia Hristian <an.hristian@gmail.com>
2020-11-04 23:10:55 -0700

    static fields and their access.

---
+++ src/edu/cnm/deepdive/testquestions/ProgramTwo.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.testquestions;
+
+public class ProgramTwo {
+
+  final int j = 32;
+
+  public static void main(String[] args) {
+    char c = 'A';
+ //   System.out.println((char) calc(c));
+  }
+
+ /* static int calc(int i) {
+//    return (i+j); will not compile because field j is not static
+}*/
+
+
+}
