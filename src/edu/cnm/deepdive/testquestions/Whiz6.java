b8664adcd8e7596750f2aad2d15e54d1ad1393d8
Anastasia Hristian <an.hristian@gmail.com>
2020-11-06 22:55:15 -0700

    do-while.

---
+++ src/edu/cnm/deepdive/testquestions/Whiz6.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive.testquestions;
+
+public class Whiz6 {
+
+  public static void main(String[] args) {
+    int x= 0;
+
+    String[] animal = new String[3];
+
+    do {
+      animal[x] = "Cat";
+      x++;
+    } while (false);
+    do {
+      animal[x] = "Dog";
+    } while (x > animal[x++].length());
+    do {
+      animal[x] = "Rat";
+    } while (x > 3);
+
+    for (String s : animal) {
+      System.out.print(s + " ");
+    }
+  }
+
+}
