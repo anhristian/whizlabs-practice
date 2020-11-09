f83dfdb4f61bfef038a799f4eb8db3643d453645
Anastasia Hristian <an.hristian@gmail.com>
2020-11-08 23:30:08 -0700

    continue and break in for/if

---
+++ src/edu/cnm/deepdive/testquestions/Whiz7.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.testquestions;
+
+public class Whiz7 {
+
+  public static void main(String[] args) {
+    int arr[][] = {{1, 3, 5}, {7,8}};
+    out: for (int[] a : arr){
+      for (int i : a){
+        if (i == 7) continue ;
+        System.out.print(i + " ");
+        if (i == 3) break ;
+        
+      }
+    }
+  }
+
+}
