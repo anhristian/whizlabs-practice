8afd89f1dfd783c6246d1e9ead028b2ae0ca903f
Anastasia Hristian <an.hristian@gmail.com>
2020-11-11 22:51:13 -0700

    practice example -interface.

---
+++ src/edu/cnm/deepdive/testquestions/A.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive.testquestions;
+
+public interface A {
+
+  static int value = 15;
+  String s = "Value is: ";
+
+}
---
+++ src/edu/cnm/deepdive/testquestions/Whiz10.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive.testquestions;
+
+public class Whiz10 implements A{
+
+  public static void main(String[] args) {
+    System.out.print(A.s);
+    System.out.print(A.value);
+  }
+
+}
