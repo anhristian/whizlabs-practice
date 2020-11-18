8d8a3412de2a436fb475831e5ff26bc309767375
Anastasia Hristian <an.hristian@gmail.com>
2020-11-17 18:26:16 -0700

    format example.

---
+++ src/edu/cnm/deepdive/prctestthree/WhizDate.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.prctestthree;
+
+import static java.lang.Math.pow;
+
+import java.util.Date;
+
+public class WhizDate {
+
+  public static void main(String[] args) {
+  //  System.out.println(new Date());
+    System.out.println(pow(5,5));
+  }
+
+}
---
+++ src/edu/cnm/deepdive/prctestthree/WhizFormat.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.prctestthree;
+
+public class WhizFormat {
+
+  public static void main(String[] args) {
+    int n = 808;
+    System.out.format("%d", n);
+    System.out.format("%06d", n);
+  }
+
+}
