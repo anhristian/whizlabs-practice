c82c760a6e5337e9014c4205605e84ee008b68bc
Anastasia Hristian <an.hristian@gmail.com>
2020-11-21 14:58:23 -0700

    practice.

---
+++ src/edu/cnm/deepdive/prctestfive/Whizlab.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.prctestfive;
+
+public class Whizlab {
+
+  public static void main(String[] args) {
+    int []a = {3,2,1,0};
+    int y = a.length;
+    while (y>=0){
+      System.out.print(a[--y]);
+    }
+  }
+
+}
