b2c4263746c9012e1e63038ab2aee5993c8e35bd
Anastasia Hristian <an.hristian@gmail.com>
2020-11-21 23:57:03 -0700

    practice.

---
+++ src/edu/cnm/deepdive/prctestfive/WhizlabOne.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.prctestfive;
+
+public class WhizlabOne {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 3; i++){
+      switch (i){
+        case 0:
+          break;
+        case 1:
+          System.out.print("1 ");
+        case 2:
+          System.out.print("2 ");
+        case 3:
+          System.out.print("3 ");
+      }
+    }
+  }
+}
---
+++ src/edu/cnm/deepdive/prctestfive/WhizlabTwo.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.prctestfive;
+
+public class WhizlabTwo {
+
+  public static void main(String[] args) {
+    Integer i1 = 100, i2 = 128;
+    Integer i3 = 128;
+    Integer i4 = 100;
+
+    if ((i1==i4) ^ (i2==i3)){
+      System.out.println(i1);
+    }else if ((i1==i4) | (i2==i3)) {
+      System.out.println(i3);
+    } else {
+      System.out.println(i3+i4);
+    }
+
+  }
+
+}
