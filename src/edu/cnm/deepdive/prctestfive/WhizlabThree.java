d1281e52587d84f014b6d3c2243e9f7176151471
Anastasia Hristian <an.hristian@gmail.com>
2020-11-22 20:26:28 -0700

    practice.

---
+++ src/edu/cnm/deepdive/prctestfive/WhizlabThree.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.prctestfive;
+
+public class WhizlabThree {
+
+  public static void main(String[] args) throws java.io.IOException{
+    StringBuilder sb = new StringBuilder("Whiz");
+    sb.append(new char[]{'l','a','b'},0,2);
+    System.out.println(sb);
+
+  }
+
+}
