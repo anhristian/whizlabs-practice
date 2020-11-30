122a44470808c924b91efdad1b2cc03635d6fab6
Anastasia Hristian <an.hristian@gmail.com>
2020-11-29 17:03:46 -0700

    practice

--- src/edu/cnm/deepdive/TestQuestion.java
+++ src/edu/cnm/deepdive/TestQuestion.java
@@ -14,5 +14,12 @@ public class TestQuestion {
       if (a.equals("2cfalse"))
         System.out.println("equals");
 
+
+      int total = 0;
+      StringBuilder letters = new StringBuilder("abcdefg");
+      total += letters.substring(1,2).length();
+      total += letters.substring(6,6).length();
+      total += letters.substring(6,5).length();
+    System.out.println(total);
     }
   }
