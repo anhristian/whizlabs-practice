aa215d90381afe8b00d9da6376d6ea74d018d21d
Anastasia Hristian <an.hristian@gmail.com>
2020-12-04 12:02:39 -0700

    practice exception.

--- src/edu/cnm/deepdive/DoSomething.java
+++ src/edu/cnm/deepdive/DoSomething.java
@@ -1,14 +1,25 @@
 package edu.cnm.deepdive;
 
 public class DoSomething {
-  public void go() {
+  public int go() {
     System.out.println("A");
-    try{
+    int a =0;
+    int b=0;
+ /*   try{
       stop();
     } catch (ArithmeticException e) {
       System.out.println("B");
     } finally {
       System.out.println("C");
+    }*/
+    try{
+      return a/b;
+    } catch (RuntimeException e){
+      return -1;
+  /*  }catch (ArithmeticException e) {
+      return 0;*/ //does not compile because this exception was already caught
+    }finally {
+      System.out.println("done");
     }
   }
   public void stop(){
