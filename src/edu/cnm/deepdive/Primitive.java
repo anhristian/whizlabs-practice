8148f549456cd7b07d8e02e794b1ce2f28cddcdc
Anastasia Hristian <an.hristian@gmail.com>
2020-12-26 16:54:00 -0700

    practice

---
+++ src/edu/cnm/deepdive/Primitive.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive;
+
+public class Primitive {
+
+  private static Character c;
+  private static float f;
+ private static boolean b;
+
+  public static void main(String[] args) {
+    System.out.println(c);
+    System.out.println(f);
+    System.out.println(b);
+  }
+
+}
